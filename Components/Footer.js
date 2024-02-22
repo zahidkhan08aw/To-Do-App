b84963386ecfabfce7cde4ae81b05a25cb7de1e5
zahidkhan08aw <webwizardzahid@gmail.com>
2024-02-22 20:39:54 +0530

    init

---
+++ Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="text-white text-center text-xl">
+        © 2024 My ToDo List. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
